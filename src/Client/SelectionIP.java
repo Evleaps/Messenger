69df47bfdaac8104bb706d742f7a3f93962cd686
Ромчи <r.aymalet@mail.ru>
2017-06-08 11:32:21 +0300

    add message history

--- src/Client/SelectionIP.java
+++ src/Client/SelectionIP.java
@@ -3,7 +3,10 @@ package Client;
 import javax.swing.*;
 
 public class SelectionIP extends JFrame {
-    private static String[] setIP = {"LocalHost: 127.0.0.1","Компьютер Ромы: 192.168.137.1", "Другой IP"};
+    private static String[] setIP = {"LocalHost: 127.0.0.1",
+            "Компьютер Ромы: 192.168.137.1",
+            "Компьютер Ромы2: 5.3.210.100",
+            "Другой IP"};
     private static ImageIcon icon = null;
 
     public void IPButton() {
@@ -18,6 +21,8 @@ public class SelectionIP extends JFrame {
                 Constant.IP = "127.0.0.1";
             } else if (selectionIP.toString ( ).equals ("Компьютер Ромы: 192.168.137.1")) {
                 Constant.IP = "192.168.137.1";
+            } else if (selectionIP.toString ( ).equals ("Компьютер Ромы2: 5.3.210.100")) {
+                Constant.IP = "Компьютер Ромы2: 5.3.210.100";
             } else {
                 Constant.IP = JOptionPane.showInputDialog (this,
                         "Введите IP-адресс подключения",
